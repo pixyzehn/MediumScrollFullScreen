7749f1388e4542f5751903d030cd15956ed1b443
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2015-02-14 18:57:23 +0530

    Setup

---
+++ MediumMenuInScroll/MediumMenuInScroll.swift
@@ -0,0 +1,15 @@
+//
+//  MediumMenuInScroll.swift
+//  MediumMenuInScroll
+//
+//  Created by pixyzehn on 2/14/15.
+//  Copyright (c) 2015 pixyzehn. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol MediumMenuInScrollDelegate {
+    
+
+    
+}
