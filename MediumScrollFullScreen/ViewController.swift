3d636e354af3fe0d7074ec9673db4d0c785d4f74
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2015-02-18 18:43:34 +0530

    Adjust paramater

--- MediumScrollFullScreen/ViewController.swift
+++ MediumScrollFullScreen/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, MediumScrollFullScreenDelegate {
+class ViewController: UIViewController, MediumScrollFullScreenDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var webView: UIWebView!
     
@@ -18,9 +18,22 @@ class ViewController: UIViewController, MediumScrollFullScreenDelegate {
         super.viewDidLoad()
         
         scrollProxy = MediumScrollFullScreen(forwardTarget: webView)
+        scrollProxy?.downThresholdY = Float.infinity
         webView.scrollView.delegate = scrollProxy
         scrollProxy?.delegate = self as MediumScrollFullScreenDelegate
-        webView.loadRequest(NSURLRequest(URL: NSURL(string: "http://www.apple.com/macbook-pro/")!))
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://medium.com/@pixyzehn/mediummenu-for-ios-5ff001f04ab5")!))
+        
+        let screenTap = UITapGestureRecognizer(target: self, action: "tapGesture:")
+        screenTap.numberOfTapsRequired = 1
+        view.addGestureRecognizer(screenTap)
+    }
+    
+    func tapGesture(sender: UITapGestureRecognizer) {
+        showNavigationBar(true)
+    }
+    
+    func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
     // MediumMenuInFullScreenDelegate
